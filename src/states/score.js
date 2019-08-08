28420dcbfcd6d5b24e58ea7408eba61c43e7d817
Ilmari <ilmari.koskinen1@gmail.com>
2019-08-08 16:00:25 +0300

    Adjust score screen

--- src/states/score.js
+++ src/states/score.js
@@ -40,7 +40,7 @@ export default class GameScene extends Phaser.Scene {
       .sort((a, b) => b.totalScore - a.totalScore)
       .forEach((player, position) => {
         const x = width * 0.1;
-        const y = 120 + position * 40;
+        const y = 120 + position * 45;
         this.add
           .text(
             x + 50,
