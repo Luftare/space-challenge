7bb123eac274c566c78a0dfa6a80d7cbffe9b764
Ilmari <ilmari.koskinen1@gmail.com>
2019-08-09 17:27:19 +0300

    Fix bug

--- src/levels/index.js
+++ src/levels/index.js
@@ -4,5 +4,6 @@ import level2 from './level-2';
 import level3 from './level-3';
 import level4 from './level-4';
 import level5 from './level-5';
+import level6 from './level-6';
 
-export default [level0, level1, level2, level3, level4, level5];
+export default [level0, level1, level2, level3, level4, level5, level6];
